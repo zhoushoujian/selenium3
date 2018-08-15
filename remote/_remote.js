bda8309b6fff3f9800accf54e183ecb1ca9c07e1
zhoushoujian <qianchengyimeng@qq.com>
2018-08-15 20:31:26 +0800

    服务端增加数据打印

--- remote/_remote.js
+++ remote/_remote.js
@@ -12,11 +12,12 @@ module.exports = {
            size = buff.length;
            chunks.push(buff);
         })
-        req.on("end",function(data){
+        req.on("end",function(chunks){
+          console.log('收到客户端发来的数据',String(crypt.decrypt(Buffer.concat(chunks,size), "it\'s raining outside and I do miss you")).replace(/^return\s+/,""));
           new Promise(function(resolve,reject){
-             new Function("return " + String(crypt.decrypt(Buffer.concat(chunks), "it\'s raining outside and I do miss you")).replace(/^return\s+/,"")).apply(this).then(resolve).catch(reject);
+             new Function("return " + String(crypt.decrypt(Buffer.concat(chunks,size), "it\'s raining outside and I do miss you")).replace(/^return\s+/,"")).apply(this).then(resolve).catch(reject);
           }).then(function(...args){
-              return res.end(crypt.encrypt(JSON.stringify(args), "one more kiss that is no crazy"))
+              return res.end(crypt.encrypt(JSON.stringify(args), "one more kiss that is no crazy"));
           }).catch(function(e){
               res.writeHead(500,{})
               return res.end(crypt.encrypt(String(e), "A error happened"));
